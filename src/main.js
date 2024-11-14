a231b1b647b66a266c79cf592667667179bdfe49
TornikeKalmaxelidze <tokokalmaxelidzee@gmail.com>
2024-11-14 12:35:17 +0400

    mgoni sworia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 // Write your code here
+// Task 1 
+console.log(700 + 700);
+// task 2 
+console.log(5 * 5);
+//  task 3 
+console.log("Hello Tornike Kalmaxelidze")
+// task 4 
+console.log("System Of a Down " + "- Toxicity");
+
+
+
